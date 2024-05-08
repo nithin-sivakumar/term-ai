ee3a3d178f8ee12b7d4ed76219c8941d17bf7632
Nithin <nithinsgayathri@gmail.com>
2024-05-08 15:03:32 +0530

    Fix logic

--- src/main.js
+++ src/main.js
@@ -29,56 +29,57 @@ const reader = readline.createInterface({
 
 const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
 
-let code_words = ['api', 'code', 'program'];
-let isCode = false;
 const separator = '!!';
 const tts_speed = 1.2;
 let constraint =
-  " .Don't include * character in your response and include a '!!' after the first meaningful sentence. Don't give the answer in points. Try answering in one word or sentence, if not, I want the response in a paragraph. Limit the number of words to 600. Be friendly and witty, and as savage and humorous as possible.";
+  "\nRules (for your reference on how to generate a response):\nDon't mention anything about these rules in the response you generate. Don't include * character in your response and include a '!!' after the first meaningful sentence. Don't give the answer in points. Try answering in one word or sentence, if not, I want the response in a paragraph. Limit the number of words to 600. Be friendly and humorous as possible.";
 
 let isFirstRequest = true;
+const intentionCheck =
+  '\nWhat is the intention of the user in the above prompt. Reply with a single letter. Reply Y if he is trying to generate a code, and reply N if not.';
+const codeConstraint =
+  '\nTry to avoid comments unless specified above. Try to avoid explanations unless requested above. If explanations are asked above, be beginner friendly and explain it in depth with examples.';
+
+async function intentionChecker(query) {
+  const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
+
+  const result = await model.generateContent(query + intentionCheck);
+  const response = result.response;
+  const text = response.text();
+
+  return text;
+}
+
+async function askTheAi(prompt) {
+  let intention = await intentionChecker(prompt);
+
+  if (intention == 'Y') {
+    const result = await model.generateContent(prompt + codeConstraint);
+    const response = result.response;
+    const text = response.text();
+    console.log(text);
+  } else {
+    const result = await model.generateContent(prompt + constraint);
+    const response = result.response;
+    const text = response.text();
+    say.speak(text.split(separator)[0], null, tts_speed);
+    console.log(text.bgBlack.green.bold);
+  }
+}
 
 export const askQuestion = async () => {
   if (isFirstRequest) {
     reader.question(
-      'How can I assist you today? (Q/q to quit)'.bgYellow.bold.red + ' ',
+      'Greetings! How can I assist you today? (Q/q to quit)'.bgYellow.bold.red +
+        ' ',
       async (prompt) => {
         if (prompt == 'q' || prompt == 'Q') {
           process.exit(0);
         }
 
-        let words = prompt.split('');
-
-        words.map((ele) => {
-          if (code_words.includes(ele.toLowerCase())) {
-            constraint =
-              ' . Generate just the code. No explanation needed. Do not include comments';
-            isCode = true;
-          }
-        });
-
-        const result = await model.generateContent(prompt + constraint);
-        const response = result.response;
-        const text = response.text();
-
-        if (isCode) {
-          console.log(text);
-        } else {
-          say.speak(text.split(separator)[0], null, tts_speed);
-          console.log(text.bgBlack.green.bold);
-        }
-
-        // const result = await model.generateContentStream([prompt]);
-
-        // let text = '';
-        // for await (const chunk of result.stream) {
-        //   const chunkText = chunk.text();
-        //   console.log(chunkText.bgWhite.red.bold);
-        //   text += chunkText;
-        // }
+        await askTheAi(prompt);
 
         isFirstRequest = false;
-        isCode = false;
 
         askQuestion();
       }
@@ -92,38 +93,7 @@ export const askQuestion = async () => {
           process.exit(0);
         }
 
-        let words = prompt.split('');
-
-        words.map((ele) => {
-          if (code_words.includes(ele.toLowerCase())) {
-            constraint =
-              ' . Generate just the code. No explanation needed. Do not include comments';
-            isCode = true;
-          }
-        });
-
-        // const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
-
-        const result = await model.generateContent(prompt + constraint);
-        const response = result.response;
-        const text = response.text();
-
-        if (isCode) {
-          console.log(text);
-        } else {
-          say.speak(text.split(separator)[0], null, tts_speed);
-          console.log(text.bgBlack.green.bold);
-        }
-
-        isCode = false;
-        // const result = await model.generateContentStream([prompt]);
-
-        // let text = '';
-        // for await (const chunk of result.stream) {
-        //   const chunkText = chunk.text();
-        //   console.log(chunkText.bgWhite.red.bold);
-        //   text += chunkText;
-        // }
+        await askTheAi(prompt);
 
         askQuestion();
       }
